4df9f33308dce57ebc685ed09cfc2b88a89127fe
filip-bestic <filip.bestic@supernovabih.ba>
2025-08-29 09:44:18 +0200

    Angular

--- skripta.js
+++
@@ -1,7 +0,0 @@
-const socket = new WebSocket('ws://localhost:8080');
-
-socket.onmessage = (event) => {
-  const value = event.data;
-  const event = new CustomEvent('input', { detail: value });
-  document.dispatchEvent(event);
-};
